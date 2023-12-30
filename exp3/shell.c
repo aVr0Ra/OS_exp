d173d2614f4892132db4b085d3a61e1158707726
aVr0Ra <ysmormichael@gmail.com>
2023-12-30 17:22:42 +0800

    Update explanations

--- exp3/shell.c
+++ exp3/shell.c
@@ -4,52 +4,54 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
-#define MAX_LENGTH 100
-#define MAX_ARGS 100
+#define MAX_LENGTH 100  // 定义最大输入长度
+#define MAX_ARGS 100    // 定义参数数组的最大长度
 
 int main() {
-    char input[MAX_LENGTH];
-    char *args[MAX_ARGS];
-    char *token;
-    pid_t pid;
-    int status = 0, i = 0;
+    char input[MAX_LENGTH]; // 用于存储输入的命令行
+    char *args[MAX_ARGS]; // 存储分割后的命令和参数
+    char *token; // 用于标记分割的字符串
+    pid_t pid; // 用于存储进程ID
+    int i = 0; // 循环变量
 
-    while (1) {
+    while (1) { // 无限循环直到用户输入 "quit\n"
         printf("21071125YSM's Shell> ");
 
+        // 读取一行输入
         if (fgets(input, MAX_LENGTH, stdin) == NULL) {
-            break;
+            break; // 如果读取失败，则跳出循环
         }
 
+        // 如果输入是 "quit\n"，退出程序
         if (strcmp(input, "quit\n") == 0) {
             break;
         }
 
+        // 使用 strtok 分割输入的命令和参数
         token = strtok(input, " \t\n");
-
         i = 0;
         while (token != NULL && i < MAX_ARGS - 1) {
             args[i++] = token;
             token = strtok(NULL, " \t\n");
         }
+        args[i] = NULL; // 确保参数数组的最后一个元素为NULL
 
-        //args last elem must be NULL
-        args[i] = NULL; 
-
+        // 创建子进程
         pid = fork();
         if (pid < 0) {
-            perror("Fork failed");
+            perror("Fork failed"); // 如果 fork 失败，打印错误并退出
             exit(1);
         }
-        else if (pid == 0) {
+        else if (pid == 0) { // 在子进程中
+            // 执行输入的命令
             if (execvp(args[0], args) < 0) {
-                perror("execvp failed");
+                perror("execvp failed"); // 如果 execvp 失败，打印错误并退出
                 exit(1);
             }
         } 
-        else {
-            wait(NULL);
-            printf("Child exited successfully\n");
+        else { 
+            wait(NULL); // 父进程等待子进程结束
+            printf("Child exited successfully\n"); // 子进程结束后打印消息
         }
     }
 
--- exp5/exp5.c
+++ exp5/exp5.c
@@ -3,111 +3,122 @@
 #include <pthread.h>
 #include <semaphore.h>
 
-#define BUFFER_SIZE 5
-#define NUMBERS 10
+#define BUFFER_SIZE 5 // 定义缓冲区大小
+#define NUMBERS 10 // 定义每个文件读取的数字数量
 
+// 定义缓冲池结构
 typedef struct {
-    int buffer[BUFFER_SIZE];
-    int in, out;
+    int buffer[BUFFER_SIZE]; // 缓冲区数组
+    int in, out; // 缓冲区的输入和输出索引
 
-    sem_t empty, full;
+    sem_t empty, full; // 信号量：empty 表示空闲位置数量，full 表示已占用位置数量
 
-    pthread_mutex_t mutex;
-}BufferPool;
+    pthread_mutex_t mutex; // 互斥锁，用于保护对缓冲区的访问
+} BufferPool;
 
-BufferPool bufferPool;
+BufferPool bufferPool; // 全局缓冲池实例
 
+// 生产者线程函数
 void *producer(void *param) {
-    int producerId = *((int *)param);
-    FILE *file = fopen(producerId == 0 ? "data1.txt" : "data2.txt", "r");
+    int producerId = *((int *)param); // 获取生产者ID
+    FILE *file = fopen(producerId == 0 ? "data1.txt" : "data2.txt", "r"); // 根据ID打开对应的文件
 
     int number;
     for (int i = 0; i < NUMBERS; i++) {
-        fscanf(file, "%d", &number);
+        fscanf(file, "%d", &number); // 从文件读取数字
 
-        sem_wait(&bufferPool.empty);
-        pthread_mutex_lock(&bufferPool.mutex);
+        sem_wait(&bufferPool.empty); // 等待缓冲区有空闲位置
+        pthread_mutex_lock(&bufferPool.mutex); // 加锁
 
-        bufferPool.buffer[bufferPool.in] = number;
-        bufferPool.in = (bufferPool.in + 1) % BUFFER_SIZE;
+        bufferPool.buffer[bufferPool.in] = number; // 将数字放入缓冲区
+        bufferPool.in = (bufferPool.in + 1) % BUFFER_SIZE; // 更新输入索引
 
-        pthread_mutex_unlock(&bufferPool.mutex);
-        sem_post(&bufferPool.full);
+        pthread_mutex_unlock(&bufferPool.mutex); // 解锁
+        sem_post(&bufferPool.full); // 通知缓冲区有新数据
     }
 
-    fclose(file);
+    fclose(file); // 关闭文件
     return NULL;
 }
 
+// 消费者线程函数（求和）
 void *consumer_sum(void *param) {
     for (int i = 0; i < BUFFER_SIZE; i++) {
         int num1, num2;
 
-        sem_wait(&bufferPool.full);
-        pthread_mutex_lock(&bufferPool.mutex);
-        num1 = bufferPool.buffer[bufferPool.out];
-        bufferPool.out = (bufferPool.out + 1) % BUFFER_SIZE;
-        pthread_mutex_unlock(&bufferPool.mutex);
-        sem_post(&bufferPool.empty);
+        // 获取第一个数字
+        sem_wait(&bufferPool.full); // 等待缓冲区有数据
+        pthread_mutex_lock(&bufferPool.mutex); // 加锁
+        num1 = bufferPool.buffer[bufferPool.out]; // 取出数字
+        bufferPool.out = (bufferPool.out + 1) % BUFFER_SIZE; // 更新输出索引
+        pthread_mutex_unlock(&bufferPool.mutex); // 解锁
+        sem_post(&bufferPool.empty); // 通知缓冲区有空闲位置
 
-        sem_wait(&bufferPool.full);
-        pthread_mutex_lock(&bufferPool.mutex);
-        num2 = bufferPool.buffer[bufferPool.out];
-        bufferPool.out = (bufferPool.out + 1) % BUFFER_SIZE;
-        pthread_mutex_unlock(&bufferPool.mutex);
-        sem_post(&bufferPool.empty);
+        // 获取第二个数字
+        sem_wait(&bufferPool.full); // 等待缓冲区有数据
+        pthread_mutex_lock(&bufferPool.mutex); // 加锁
+        num2 = bufferPool.buffer[bufferPool.out]; // 取出数字
+        bufferPool.out = (bufferPool.out + 1) % BUFFER_SIZE; // 更新输出索引
+        pthread_mutex_unlock(&bufferPool.mutex); // 解锁
+        sem_post(&bufferPool.empty); // 通知缓冲区有空闲位置
 
-        printf("Sum: %d + %d = %d\n", num1, num2, num1 + num2);
+        printf("Sum: %d + %d = %d\n", num1, num2, num1 + num2); // 打印和
     }
     return NULL;
 }
 
+// 消费者线程函数（求积）
 void *consumer_product(void *param) {
     for (int i = 0; i < BUFFER_SIZE; i++) {
         int num1, num2;
 
-        sem_wait(&bufferPool.full);
-        pthread_mutex_lock(&bufferPool.mutex);
-        num1 = bufferPool.buffer[bufferPool.out];
-        bufferPool.out = (bufferPool.out + 1) % BUFFER_SIZE;
-        pthread_mutex_unlock(&bufferPool.mutex);
-        
-        sem_post(&bufferPool.empty);
+        // 获取第一个数字
+        sem_wait(&bufferPool.full); // 等待缓冲区有数据
+        pthread_mutex_lock(&bufferPool.mutex); // 加锁
+        num1 = bufferPool.buffer[bufferPool.out]; // 取出数字
+        bufferPool.out = (bufferPool.out + 1) % BUFFER_SIZE; // 更新输出索引
+        pthread_mutex_unlock(&bufferPool.mutex); // 解锁
+        sem_post(&bufferPool.empty); // 通知缓冲区有空闲位置
 
-        sem_wait(&bufferPool.full);
-        pthread_mutex_lock(&bufferPool.mutex);
-        num2 = bufferPool.buffer[bufferPool.out];
-        bufferPool.out = (bufferPool.out + 1) % BUFFER_SIZE;
-        pthread_mutex_unlock(&bufferPool.mutex);
+        // 获取第二个数字
+        sem_wait(&bufferPool.full); // 等待缓冲区有数据
+        pthread_mutex_lock(&bufferPool.mutex); // 加锁
+        num2 = bufferPool.buffer[bufferPool.out]; // 取出数字
+        bufferPool.out = (bufferPool.out + 1) % BUFFER_SIZE; // 更新输出索引
+        pthread_mutex_unlock(&bufferPool.mutex); // 解锁
+        sem_post(&bufferPool.empty); // 通知缓冲区有空闲位置
 
-        sem_post(&bufferPool.empty);
-
-        printf("Product: %d * %d = %d\n", num1, num2, num1 * num2);
+        printf("Product: %d * %d = %d\n", num1, num2, num1 * num2); // 打印积
     }
     return NULL;
 }
 
+// 主函数
 int main() {
-    pthread_t tidA, tidB, tidC, tidD;
-    int producerId1 = 0, producerId2 = 1;
+    pthread_t tidA, tidB, tidC, tidD; // 线程标识符
+    int producerId1 = 0, producerId2 = 1; // 生产者ID
 
+    // 初始化信号量和互斥锁
     sem_init(&bufferPool.empty, 0, BUFFER_SIZE);
     sem_init(&bufferPool.full, 0, 0);
     pthread_mutex_init(&bufferPool.mutex, NULL);
 
-    bufferPool.in = 0;
-    bufferPool.out = 0;
+    bufferPool.in = 0; // 初始化输入索引
+    bufferPool.out = 0; // 初始化输出索引
 
+    // 创建线程
     pthread_create(&tidA, NULL, producer, &producerId1);
     pthread_create(&tidB, NULL, producer, &producerId2);
     pthread_create(&tidC, NULL, consumer_sum, NULL);
     pthread_create(&tidD, NULL, consumer_product, NULL);
 
+    // 等待线程结束
     pthread_join(tidA, NULL);
     pthread_join(tidB, NULL);
     pthread_join(tidC, NULL);
     pthread_join(tidD, NULL);
 
+    // 销毁互斥锁和信号量
     pthread_mutex_destroy(&bufferPool.mutex);
     sem_destroy(&bufferPool.empty);
     sem_destroy(&bufferPool.full);
